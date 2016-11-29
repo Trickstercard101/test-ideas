1c9664c1e345bcc9dde715a7ed71965a15f3bbed
Trickstercard101 <boearles@gmail.com>
2016-11-28 21:21:02 -0600

    Add files via upload

---
+++ VariableTesting.java
@@ -0,0 +1,7 @@
+public class VariableTesting {
+  public static void main(String[] args) {
+    int firstInt = 2;
+    int secondInt = firstInt*firstInt;
+    System.out.println(firstInt + " squared equals " + secondInt);
+  }
+}
